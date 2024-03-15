2ed8e34344360ba8e0a040f1551567354de13c1f
sherryyangyzt <142111351+sherryyangyzt@users.noreply.github.com>
2024-03-15 19:36:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+name_flag = 0
+name=""
+
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    name=request.form.get("name")    
+    global name_flag,name
+    if name_flag==0:        
+        name=request.form.get("name")    
+        name_flag=1
+        
     return(render_template("main.html",name=name))
 
 @app.route("/ethical_test",methods=["GET","POST"])
